c958339ff4c758aae78dcb47a729db5be35b904c
Ahnaf Sadik Inan <ahnafsadik-2018725375@math.du.ac.bd>
2024-07-02 13:31:34 +0600

    First data structure complete
    Linked List

--- linked_list.py
+++ linked_list.py
@@ -48,3 +48,112 @@ class LinkedList:
         new_node = Node(data)
         new_node.next_node = self.head
         self.head = new_node
+        
+        
+    def search(self, key):
+        
+        """
+        Search for the first node that contains the data
+        if not found returns a not found statement
+        
+        Takes O(n) runtime
+        """
+        
+        current = self.head
+        
+        while current:
+            if current.data == key:
+                return current
+            else:
+                current = current.next_node
+        
+        return '<The value %d was not found in the linked list!>' %key
+    
+    
+    def insert(self, data, index):
+        
+        """
+        Inserts data to the specified index
+        
+        Insertion takes constant time
+        getting to the index takes O(n) time
+        """
+        
+        current = self.head
+        
+        count = 0
+        
+        data = Node(data)
+        
+        while current:
+            
+            if index == 0:
+                data.next_node = self.head
+                self.head = data
+                
+                return None
+            else:
+                
+                if count == index-1:
+                    data.next_node = current.next_node
+                    current.next_node = data
+                    
+                    return None
+            
+            current = current.next_node
+            count +=1
+            
+    def remove(self, key):
+        current = self.head
+        
+        if key == current.data:
+            self.head = current.next_node
+        
+        while current:
+            
+            if key == current.data:
+                prev.next_node = current.next_node
+            
+            prev = current
+            current = current.next_node
+            
+        
+        
+    def __repr__(self) -> str:
+        """
+        returns a string of the linked list
+        marks the head and tail
+        """
+        
+        current = self.head
+        
+        text = ""
+        
+        while current != None:
+            
+            if current == self.head:
+                text = text + "[Head:] " + str(current.data)
+                
+            elif current.next_node == None:
+                text = text + " -> [Tail:] " + str(current.data)
+                
+            else:
+                text = text + " -> " + str(current.data)
+            
+            current = current.next_node
+            
+        return text
+    
+    
+l = LinkedList()
+l.add(1)
+l.add(2)
+l.add(3)
+l.add(4)
+l.add(5)
+l.add(6)
+l.add(7)
+print(l.search(6))
+l.insert(50, 6)
+l.remove(5)
+print(l)
